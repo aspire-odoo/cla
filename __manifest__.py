8b3a0a99c2328e08559a972de4661332ed8cbbc8
aspire-odoo <123382260+aspire-odoo@users.noreply.github.com>
2023-02-08 08:50:55 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'CleanLight Vendor Report',
+    'version': '1.0',
+    'category': '',
+    'summary': """module changes vendor report""",
+
+    'license': 'LGPL-3',
+    'depends': ['l10n_cl'],
+    'data': [
+        'report_vendor_bill.xml',
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
